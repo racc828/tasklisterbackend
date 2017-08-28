8279ef07e3b79634c2f39877b13bb618de996ba1
github username <github email address>
2017-08-28 14:26:20 -0400

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,18 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def authorize_user!
-    if !current_user.present?
-      render json: { error: 'No user id present' }
-    end
-  end
-
   def current_user
     current_user ||= User.find_by(id: user_id)
   end
 
   def user_id
     decoded_token.first['id']
+  end
+
+  def authorize_user!
+    if !current_user.present?
+      render json: { error: 'No user id present' }
+    end
   end
 
   def decoded_token
